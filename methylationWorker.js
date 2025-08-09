2a2a9aefa047cf57e341a8137d2767827c232c93
jalivald2-cell <jalivald2@gmail.com>
2025-08-09 11:11:42 +0300

    Update methylationWorker.js

--- methylationWorker.js
+++ methylationWorker.js
@@ -1,22 +1,6 @@
-const BASES = ['A','T','C','G'];
-
-function baseAt(pos) {
-  const val = (Math.sin(pos * 12.9898 + 78.233) * 43758.5453) % 4;
-  const idx = Math.abs(Math.floor(val));
-  return BASES[idx];
-}
-
-function isCpG(pos) {
-  return baseAt(pos) === 'C' && baseAt(pos+1) === 'G';
-}
-
-onmessage = function(e) {
-  const {start, length} = e.data;
-  const result = new Array(length).fill(false);
-  for(let i=0; i<length; i++) {
-    if(isCpG(start + i)) {
-      result[i] = true;
-    }
-  }
-  postMessage({start, length, methylation: result});
-}
+self.onmessage = function(e) {
+  const pos = e.data.position;
+  // חישוב דמה: אם הבסיס הוא C, יש מתילציה (true), אחרת false
+  const methylated = (pos % 4 === 0);
+  self.postMessage({ position: pos, methylated });
+};
